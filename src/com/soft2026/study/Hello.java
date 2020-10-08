434148dd197a312585d6167991645405c45d76c5
hanxinjie04 <1402456998@qq.com>
2020-10-08 19:10:12 +0800

    ”””初始化“

---
+++ src/com/soft2026/study/Hello.java
@@ -0,0 +1,13 @@
+package com.soft2026.study;
+/**
+ *@ClassName Hello
+ *@Description TODO
+ *@Author 逗你玩
+ *@Date 2020/10/8
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/com/soft2026/study/week1/Tets.java
@@ -0,0 +1,13 @@
+package com.soft2026.study.week1;
+
+/**
+ * @ClassName Tets
+ * @Description TODO
+ * @Author 韩辛杰
+ * @Date 2020/10/8
+ **/
+
+public class Tets {
+
+
+}
